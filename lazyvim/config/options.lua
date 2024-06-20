c8bb0045a3bc9f55ed50fb5cbf7cd7743d8a85b3
peterglezroa <peterglezroa@gmail.com>
2024-06-20 11:52:51 -0600

    Commit changes I made after trying lazyvim... Didn't like it

--- lazyvim/config/options.lua
+++ lazyvim/config/options.lua
@@ -4,3 +4,31 @@
 
 -- Disable annoying auto
 vim.g.autoformat = false
+
+-- Set Tabstops
+--vim.opt.tabstop = 4
+--vim.opt.shiftwidth = 4
+--vim.opt.softtabstop = 4
+--vim.opt.expandtab = true
+--vim.opt.hlsearch = true
+--vim.opt.smarttab = false
+
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.softtabstop = 2
+vim.opt.expandtab = true
+vim.opt.hlsearch = true
+vim.opt.smarttab = false
+
+-- Dynamic tab stops
+--vim.api.nvim_create_autocmd("FileType", {
+--  pattern = { "py" },
+--  callback = function()
+--    vim.opt.tabstop = 2
+--    vim.opt.shiftwidth = 2
+--    vim.opt.softtabstop = 2
+--  end,
+--})
+
+-- 80 char line visual aid
+
